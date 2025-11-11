eed4f80a45a1f7c565acabdb8fd90443b86fa02b
Felix Konprecht <fkonprecht@gmail.com>
2025-11-11 12:36:59 +0100

    added all planned program options except the ones for sorting
    - split code into several functions
    - moved map to global scope (maybe wrap in class in the future)

--- main.cpp
+++ main.cpp
@@ -1,24 +1,91 @@
 #include <iostream>
 #include <boost/program_options.hpp>
 
-int main(const int argc, char** argv) {
-    // an options_description defines what arguments can be passed to the program
-    boost::program_options::options_description description("Allowed options");
-    description.add_options()
-        ("help,h", "Display help message");
 
-    // map where the parsed option information is stored
-    boost::program_options::variables_map map;
+// map where the parsed option information is stored
+boost::program_options::variables_map map;
+
+boost::program_options::options_description parse_options(int argc, char** argv);
+int error_handling();
+// debug functions
+void print_options();
+
+int main(const int argc, char** argv) {
+    const auto options = parse_options(argc, argv);
+
+    if (map.contains("help")) {
+        std::cout << options << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    const int error_code = error_handling();
+    if (error_code != EXIT_SUCCESS)
+        return error_code;
+
+    print_options();
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
+
+boost::program_options::options_description parse_options(const int argc, char** argv) {
+    // an options_description defines what arguments can be passed to the program
+    boost::program_options::options_description options("Allowed options");
+    options.add_options()
+        ("help,h", "Display help message")
+        ("path,p", boost::program_options::value<std::string>(), "Path to the folder that should be scanned")
+        ("name,n", boost::program_options::value<std::string>(), "Name or regex of files to search for")
+        ("min-size", boost::program_options::value<size_t>(), "Minimum file size in bytes")
+        ("max-size", boost::program_options::value<size_t>(), "Maximum file size in bytes (0 for no limit)")
+        ("before,b", boost::program_options::value<std::string>(), "Find files modified before the given date (YYYY-MM-DD)")
+        ("after,a", boost::program_options::value<std::string>(), "Find files modified after the given date (YYYY-MM-DD)");
+
+    // makes the "path" option implicit
+    boost::program_options::positional_options_description positional_options;
+    positional_options.add("path", -1);
+
     // the parse_command_line function parses the arguments into usable format. the object itself is not ment to persistent
-    const auto parser = boost::program_options::parse_command_line(argc, argv, description);
+    const auto parser = boost::program_options::command_line_parser(argc, argv)
+                                                                      .options(options)
+                                                                      .positional(positional_options)
+                                                                      .run();
     // actually sore information into the map
     boost::program_options::store(parser, map);
 
-    if (map.contains("help")) {
-        std::cout << description << std::endl;
-        return 0;
+    return options;
+}
+
+int error_handling() {
+    // make the program exit if there is no path...
+    if (!map.contains("path")) {
+        std::cerr << "Error: No path specified!" << std::endl;
+        return EXIT_FAILURE;
     }
 
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // ...or if there are no search criteria
+    const auto options_count = map.count("name") +
+                        map.count("min-size") +
+                        map.count("max-size") +
+                        map.count("before") +
+                        map.count("after");
+    if (options_count <= 0) {
+        std::cerr << "Error: No search criteria specified!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void print_options() {
+    if (map.contains("path"))
+        std::cout << "Path: " << map["path"].as<std::string>() << std::endl;
+    if (map.contains("name"))
+        std::cout << "Name: " << map["name"].as<std::string>() << std::endl;
+    if (map.contains("min-size"))
+        std::cout << "Min Size: " << map["min-size"].as<size_t>() << std::endl;
+    if (map.contains("max-size"))
+        std::cout << "Maximum Size: " << map["max-size"].as<size_t>() << std::endl;
+    if (map.contains("before"))
+        std::cout << "Before: " << map["before"].as<std::string>() << std::endl;
+    if (map.contains("after"))
+        std::cout << "After: " << map["after"].as<std::string>() << std::endl;
 }
